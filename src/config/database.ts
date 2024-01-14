7b11bf0569e33c791685799f7d6eace3ad206f9e
Navin Kumar <navin.kumar@NONSMTP.rhbgroup.com>
2024-01-15 00:20:58 +0800

    Post man collection

--- src/config/database.ts
+++ src/config/database.ts
@@ -15,3 +15,23 @@ const sequelize = new Sequelize(dbConfig.database, dbConfig.user, dbConfig.passw
 });
 
 export default sequelize;
+
+// import { Sequelize } from 'sequelize';
+
+// const dbConfig = {
+//   host: process.env.DB_HOST || 'localhost',
+//   user: process.env.DB_USER || 'your_postgres_user',
+//   password: process.env.DB_PASSWORD || 'your_postgres_password',
+//   database: process.env.DB_DATABASE || 'your_postgres_database',
+// };
+
+// const sequelize = new Sequelize({
+//   dialect: 'postgres',
+//   host: dbConfig.host,
+//   username: dbConfig.user,
+//   password: dbConfig.password,
+//   database: dbConfig.database,
+// });
+
+// export default sequelize;
+
